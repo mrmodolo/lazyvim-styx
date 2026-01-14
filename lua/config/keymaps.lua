4b1be92810ab5f2b0a7cba2dcdd6194af5b5cb39
mrmodolo <marcelo.rmodolo@gmail.com>
2026-01-14 15:22:23 -0300

    fix(keymaps):atualizar para snacs

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,9 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-local Util = require("lazyvim.util")
 
 -- Mapeia <leader>sc para abrir o Chicken Scheme num terminal flutuante
 vim.keymap.set("n", "<leader>sc", function()
-  Util.terminal({ "csi" }, { cwd = Util.root(), border = "rounded" })
+  require("snacks").terminal({ "csi" }, { cwd = vim.fn.getcwd(), border = "rounded" })
 end, { desc = "Chicken Scheme REPL" })
